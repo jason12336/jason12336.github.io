74e0b5de53c9d872b35b131f5ff8f969cfc3ac13
Jason <jason.madden8@mail.dcu.ie>
2016-06-09 23:49:12 +0100

    Update animation.js

--- circleAnimation/animation.js
+++ circleAnimation/animation.js
@@ -3,7 +3,7 @@ var obArray = [];
 var x = 0;
 var rate;
 function setup() {
-	var cvs = createCanvas(windowWidth * 0.6, windowHeight * 0.6);
+	var cvs = createCanvas(0, windowHeight * 0.6);
 	cvs.position((windowWidth / 2) - width / 2, 65);
 	rate = createSlider(1, 20, 1);
 	rate.position(0, height + 85);
